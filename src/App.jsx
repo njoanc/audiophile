3ad93ab4d2140afe8c8f4f7300c745329fc01729
Jeanne d'Arc NYIRANWIZA <njoanc@gmail.com>
2024-06-05 12:31:02 +0200

    feat():pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import Header from "./components/Header";
+import { useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Nav from "./components/shared/Nav";
+import Home from "./components/pages/home/Home";
+import About from "./components/shared/About";
+import Footer from "./components/shared/Footer";
+
+// pages
+import Headphones from "./components/pages/Headphones";
+import Speakers from "./components/pages/Speakers";
+import Earphones from "./components/pages/Earphones";
+import ProductDetails from "./components/pages/ProductDetails";
+import Checkout from "./components/pages/Checkout";
 
 const App = () => {
+  const [cartItemCount, setCartItemCount] = useState(0);
+  const location = useLocation();
+  const hideAbout = location.pathname === "/checkout";
+
   return (
-    <>
-      <Header />
-    </>
+    <div className="App">
+      <Nav cartItemCount={cartItemCount} setCartItemCount={setCartItemCount} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/headphones" element={<Headphones />} />
+        <Route path="/speakers" element={<Speakers />} />
+        <Route path="/earphones" element={<Earphones />} />
+        <Route
+          path="/products/:slug"
+          element={<ProductDetails setCartItemCount={setCartItemCount} />}
+        />
+
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+      {!hideAbout && <About />}
+      <Footer />
+    </div>
   );
 };
 
---
+++ src/components/pages/Speakers.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ProductTitleHeader from "../shared/ProductTitleHeader";
+import ProductPreviewCard from "../shared/ProductPreviewCard";
+import productData from "../../data/products.json";
+import ProductCategory from "../shared/ProductCateogry";
+
+function Speakers() {
+  const speakers = productData.filter(
+    (product) => product.category === "speakers"
+  );
+
+  return (
+    <div>
+      <ProductTitleHeader title="Speakers" />
+      <div className="flex flex-col gap-[120px] lg:mb-[120px]">
+        {speakers
+          .sort((a, b) => b.new - a.new || a.id - b.id)
+          .map((speaker, index) => (
+            <ProductPreviewCard
+              key={speaker.id}
+              image={speaker.categoryImage}
+              name={speaker.name}
+              description={speaker.description}
+              newProduct={speaker.new}
+              id={speaker.id}
+              slug={speaker.slug}
+              isReversed={index % 2 === 1}
+            />
+          ))}
+      </div>
+      <ProductCategory />
+    </div>
+  );
+}
+
+export default Speakers;
